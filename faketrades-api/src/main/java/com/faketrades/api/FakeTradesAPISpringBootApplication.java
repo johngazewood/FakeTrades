59ccb9b5dc356d924b7a5849d92bb2150f8fda32
JP <2518gazewood@gmail.com>
2020-07-03 10:33:06 -0400

    Add logging.

--- faketrades-api/src/main/java/com/faketrades/api/FakeTradesAPISpringBootApplication.java
+++ faketrades-api/src/main/java/com/faketrades/api/FakeTradesAPISpringBootApplication.java
@@ -1,12 +1,18 @@
 package com.faketrades.api;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FakeTradesAPISpringBootApplication {
 
+	private static final Logger logger = LogManager.getLogger(FakeTradesAPISpringBootApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("--------------------------------------------------------");
+		logger.info("Starting Spring Application.");
 		SpringApplication.run(FakeTradesAPISpringBootApplication.class, args);
 	}
 }
--- faketrades-api/src/main/java/com/faketrades/api/heartbeat/HeartBeatController.java
+++ faketrades-api/src/main/java/com/faketrades/api/heartbeat/HeartBeatController.java
@@ -1,5 +1,7 @@
 package com.faketrades.api.heartbeat;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/heartbeat")
 public class HeartBeatController {
 
+	private static final Logger logger = LogManager.getLogger(HeartBeatController.class);
+
 	@GetMapping(path = "", produces = "application/json")
 	public String getHeartBeat() {
+		logger.info("GET /heartbeat requested.");
 		return "The heart beats back";
 	}
 
